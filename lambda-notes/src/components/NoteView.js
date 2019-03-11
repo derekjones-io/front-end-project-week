b8501ebe9372d3bfca1884ce84e9f51f979fd380
Derek Jones <derek@derekjones.io>
2019-03-11 19:58:31 -0400

    Modal shows when delete is clicked

--- lambda-notes/src/components/NoteView.js
+++ lambda-notes/src/components/NoteView.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { Button, Modal, ModalBody } from 'reactstrap';
 
 // import '../styles/noteview.css';
 
 class NoteView extends Component {
+  state = { modal: false };
+
   handleDeleteNote = event => {
     event.preventDefault();
 
     this.props.deleteNote(this.props.match.params.id);
 
     this.props.history.push('/');
+  };
+
+  toggle = () => {
+    this.setState({ modal: !this.state.modal });
   };
 
   render() {
@@ -20,7 +27,7 @@ class NoteView extends Component {
             <Link to={`/edit/${this.props.match.params.id}`}>edit</Link>
           </p>
           <p className="edit-delete-text">
-            <a onClick={this.handleDeleteNote}>delete</a>
+            <a onClick={this.toggle}>delete</a>
           </p>
         </div>
         {this.props.notes.map(note => {
@@ -33,6 +40,32 @@ class NoteView extends Component {
             );
           }
         })}
+        <Modal
+          isOpen={this.state.modal}
+          toggle={this.toggle}
+          className={this.props.className}
+          backdrop={this.state.backdrop}
+        >
+          <ModalBody className="modal-container">
+            <p>Are you sure you want to delete this?</p>
+            <div>
+              <Button
+                className="modal-button"
+                color="danger"
+                onClick={this.handleDeleteNote}
+              >
+                Delete
+              </Button>{' '}
+              <Button
+                className="modal-button"
+                color="primary"
+                onClick={this.toggle}
+              >
+                No
+              </Button>
+            </div>
+          </ModalBody>
+        </Modal>
       </div>
     );
   }
